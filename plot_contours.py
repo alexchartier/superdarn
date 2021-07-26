f5d4ad6179ac048b688a540a4467116aa8af5c45
dsitaram <sdevasena@gmail.com>
2021-07-26 14:08:26 -0400

    updated processing and plotting
    process_dmsp: processes the SSIES data into pickle files
    plot_contours: plots model velocities + contour plots. transforms/interpolates the velocity vectors

--- plot_contours.py
+++ plot_contours.py
@@ -9,31 +9,32 @@ import nc_utils
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import pickle
+from scipy.interpolate import griddata
+import nvector as nv
+import cartopy.crs as ccrs
+
 
 #read data
-pot_file = "sami3_may23_phi.nc"
-vel_file = "sami3_may23a.nc"
-sat_file = ""
+day = 23
+pot_file = "sami3_may%i_phi.nc" % (day)
+vel_file = "sami3_may%ia.nc" % (day)
 
 pot_data = nc_utils.ncread_vars(pot_file)
 vel_data = nc_utils.ncread_vars(vel_file)
 
+
 date = pot_file.split("_")[1]
-timeIndex = 50
-phi = pot_data["phi"][timeIndex,:,:]
-hour = str(pot_data["time"][timeIndex])
-
-#change potential data to polar and plot
-adjPotLats = 90 - pot_data["lat"]
-adjPotLons = np.deg2rad(pot_data["lon"])
-
-fig, ax = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
+modTimeIndex = 0
+phi = pot_data["phi"][modTimeIndex,:,:]
+hour = str(pot_data["time"][modTimeIndex])
+dmspTindex = int(float(hour)*3600)
 
 
-pot = ax[0].contour(adjPotLons, adjPotLats, phi, cmap='hot')
-ax[0].set_theta_zero_location("E")
-ax[0].set_rlabel_position(50)
 
+
+#plot model velocity data
+fig, ax = plt.subplots(1, 1, subplot_kw=dict(projection='polar'))
 
 #change model velocity data to polar
 poleIndex = vel_data["lat0"] >= 70
@@ -41,21 +42,113 @@ adjVelLats = (90 - vel_data["lat0"][poleIndex]).flatten()
 adjVelLons = np.deg2rad(vel_data["lon0"][poleIndex]).flatten()
 
 #more polar conversions
-vertical = vel_data["u1p0"][timeIndex][poleIndex]
-horizontal = vel_data["u3h0"][timeIndex][poleIndex]
-theta = np.arctan2(vertical,horizontal)
-mag = np.hypot(vertical, horizontal)
+mod_vertical = vel_data["u1p0"][modTimeIndex][poleIndex]
+mod_horizontal = vel_data["u3h0"][modTimeIndex][poleIndex]
+
 
 #plot model velocity data
-vel = ax[1].quiver(adjVelLons, adjVelLats, mag, theta)
-ax[1].set_rlabel_position(50)
-
-cbar = fig.colorbar(pot)
+vel = ax.quiver(adjVelLons, adjVelLats, mod_horizontal, mod_vertical)
+ax.set_rlabel_position(50) 
 title = ("AMPERE/SAMI3 %s %s UT" % (date, hour))
 plt.suptitle(title)
 
 fig.tight_layout()
 
-plt.savefig("test.png", dpi = 300)
+plt.savefig("test_vel.png", dpi = 300)
 plt.show()
 plt.close()
+
+
+
+
+#change potential data to polar and plot
+adjPotLats = 90 - pot_data["lat"]
+adjPotLons = np.deg2rad(pot_data["lon"])
+
+# subplot_kw=dict(projection='polar')
+fig, ax = plt.subplots(1, 1, subplot_kw=dict(projection='polar'))
+
+pot = ax.contour(adjPotLons, adjPotLats, phi, cmap='hot')
+ax.set_theta_zero_location("E")
+ax.set_rlabel_position(50)
+cbar = fig.colorbar(pot)
+
+
+plt.show()
+plt.close()
+
+colors = ["red", "orange", "green", "blue", "magenta"]
+fig, ax = plt.subplots(1, 1)
+
+#compare data
+for sat in range(16,19):
+    
+    print(sat)
+
+    sat_file = "dms_ut_201405%i_%i.002.p" % (day, sat)
+    dmsp_data = pickle.load( open(sat_file, "rb"))
+    
+    satLat = dmsp_data["GDLAT"][dmspTindex]
+    satLon = dmsp_data["GLON"][dmspTindex]
+    forward = dmsp_data["ION_V_SAT_FOR"][dmspTindex]
+    left = dmsp_data["ION_V_SAT_LEFT"][dmspTindex]
+    
+    #create vectors based on satellite direction
+    wgs84 = nv.FrameE(name='WGS84')
+    init = wgs84.GeoPoint(dmsp_data["GDLAT"][dmspTindex], 
+                          dmsp_data["GLON"][dmspTindex], 0, degrees=True)
+    
+    final = wgs84.GeoPoint(dmsp_data["GDLAT"][dmspTindex + 1], 
+                          dmsp_data["GLON"][dmspTindex + 1], 0, degrees=True)
+    
+    p_AB_N = init.delta_to(final) 
+    azimuth = p_AB_N.azimuth_deg
+    north, east, z = p_AB_N.pvector.ravel()
+    
+    
+    #use geometry to find components of dmsp measurement velocity
+    theta_for = np.arctan2(north, east)
+    alpha = np.arctan(abs(forward) / abs(left))
+    theta = theta_for + np.pi/2 - alpha
+    mag = np.hypot(forward, left)
+
+    #dmsp measurement components
+    dmsp_east = mag * np.cos(theta)
+    dmsp_north = mag * np.sin(theta)
+    
+
+    #interpolate model data between satellite coordinates
+    interp_mod_x = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
+                            vel_data["u3h0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
+    interp_mod_y = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
+                            vel_data["u1p0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
+    
+
+    print(interp_mod_x)
+    print(interp_mod_y)
+    print(dmsp_east)
+    print(dmsp_north)
+    
+    #ax.quiver(dmsp_data["GLON"][dmspTindex], dmsp_data["GDLAT"][dmspTindex], 
+    #          interp_mod_x, interp_mod_y, color = colors[sat -15])
+    
+
+    
+    
+
+title = ("AMPERE/SAMI3 %s %s UT" % (date, hour))
+plt.suptitle(title)
+
+fig.tight_layout()
+
+plt.savefig("test_pot.png", dpi = 300)
+plt.show()
+plt.close()
+
+
+
+
+
+
+
+

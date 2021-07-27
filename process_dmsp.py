2eb4c433bd088cecd0701c11bb2eb5aacc0ff1a8
Alex Chartier <alex.chartier@outlook.com>
2021-07-27 10:35:28 -0400

    merge

--- process_dmsp.py
+++ process_dmsp.py
@@ -17,69 +17,54 @@ import matplotlib.pyplot as plt
 import os
 from os import path
 
-def read_data():
-    
-    
+def read_data(
     directory = "/Users/sitardp1/Documents/Madrigal"
-    
+):
     for entry in os.scandir(directory):
         print(entry)
-        picklename = entry.name.replace(".hdf5","")+".p"
+        picklename = entry.name.replace(".hdf5","") + ".pkl"
         
         if entry.path.endswith(".hdf5") and not(path.exists(picklename)):
-
             with h5py.File(entry, "r") as f:
-                 
                 # Get the metadata
                 vn = f.get('Metadata')
-                parameters = vn['Data Parameters']
-                #print(vn['Data Parameters'][...])
                     
                 # Get the data
                 data = f.get('Data')
                 dataTable = data.get('Table Layout')[...]
                         
-                dmsp_data = {"UT1_UNIX":np.array([]), "UT2_UNIX":np.array([]), 
-                             "GDLAT":np.array([]), "GLON":np.array([]), "HOR_ION_V":np.array([]), 
-                             "VERT_ION_V":np.array([])}
-                
-                #put data in a dictionary
-                try:
-                    if str(parameters[20][0]) == "b'VERT_ION_V'": 
+            parameters = vn['Data Parameters']
+            dmsp_data = {"UT1_UNIX":np.array([]), "UT2_UNIX":np.array([]), 
+                         "GDLAT":np.array([]), "GLON":np.array([]), "HOR_ION_V":np.array([]), 
+                         "VERT_ION_V":np.array([])}
             
-                        for line in dataTable:
+            #put data in a dictionary
+            if str(parameters[20][0]) == "b'VERT_ION_V'": 
+                for line in dataTable:
+                    if np.isnan(line[20]) == False:
+                        for var in range(9, 13):
+                            key = str(parameters[var][0])
+                            key = key.replace("b'", "")
+                            key = key.replace("'", "")
+                            dmsp_data[key] = np.append(dmsp_data[key], line[var])
+            
+                        for var in range(19, 21):
+                            key = str(parameters[var][0])
+                            key = key.replace("b'", "")
+                            key = key.replace("'", "")
+                            dmsp_data[key] = np.append(dmsp_data[key], line[var])
+                with open(picklename, "wb") as f:
+                    pickle.dump(dmsp_data, f)
                             
-                            if np.isnan(line[20]) == False:
-            
-                                for var in range(9, 13):
-                                    
-                                    key = str(parameters[var][0])
-                                    key = key.replace("b'", "")
-                                    key = key.replace("'", "")
-                                    dmsp_data[key] = np.append(dmsp_data[key], line[var])
-                    
-                                for var in range(19, 21):
-                                    
-                                    key = str(parameters[var][0])
-                                    key = key.replace("b'", "")
-                                    key = key.replace("'", "")
-                                    dmsp_data[key] = np.append(dmsp_data[key], line[var])
-                    
-                        
-                        pickle.dump(dmsp_data, open( picklename, "wb" ) )
-                                    
-                except IndexError or ValueError:
-                    print("eh not a file we want")
 
 
 def plot_satellitedata(dates):
     with open("dms_20140423_18s1.001.p", 'rb') as f:
         data = pickle.load(f)
-    
-def main():
-    read_data()    
+    breakpoint() 
     
     
-main()
+if __name__ == '__main__':
+    read_data()
 
 

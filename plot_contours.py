58fd6965083f74ea162d4c507c9997b7d50d5358
dsitaram <sdevasena@gmail.com>
2021-07-28 10:45:38 -0400

    Update plot_contours.py

--- plot_contours.py
+++ plot_contours.py
@@ -35,7 +35,7 @@ def main():
     #plot sami3 velocities, potential contours, and transform dmsp vector data
     plot_model_vel(vel_data, day, modTimeIndex)
     plot_model_contour(pot_data, day, modTimeIndex)
-    vector_transform(vel_data, modTimeIndex, day)
+    vector_transform(vel_data, modTimeIndex, day, 15)
 
 #returns the index of an array which is closest to value    
 def nearest_index(value, array):
@@ -104,71 +104,66 @@ def plot_model_contour(pot_data, day, modTimeIndex):
 
 
 
-def vector_transform(vel_data, modTimeIndex, day):
+def vector_transform(vel_data, modTimeIndex, day, sat):
 
     fig, ax = plt.subplots(1, 1)
+    
+    #read data
+    sat_file = "dms_ut_201405%i_%i.002.p" % (day, sat)
+    dmsp_data = pickle.load( open(sat_file, "rb"))
+    
+    #quality flags for data
+    forQualIndex = dmsp_data["ION_V_FOR_FLAG"] == 1
+    leftQualIndex = dmsp_data["ION_V_LEFT_FLAG"] == 1
+    qualFlag = forQualIndex & leftQualIndex
+    
+    #set time index for dmsp data
+    startDay = datetime(2014, 5, day)
+    dayUnix = time.mktime(startDay.timetuple())
+    timeUnix = dayUnix + vel_data["time"][modTimeIndex]*3600
+    dmspTindex = nearest_index(timeUnix, dmsp_data["UT1_UNIX"][qualFlag])
+    
+    #read satellite data
+    satLat = dmsp_data["GDLAT"][qualFlag][dmspTindex]
+    satLon = dmsp_data["GLON"][qualFlag][dmspTindex]
+    forward = dmsp_data["ION_V_SAT_FOR"][qualFlag][dmspTindex]
+    left = dmsp_data["ION_V_SAT_LEFT"][qualFlag][dmspTindex]
+    
+    #create north/east vectors based on satellite direction
+    wgs84 = nv.FrameE(name='WGS84')
+    init = wgs84.GeoPoint(satLat, 
+                          satLon, 0, degrees=True)
+    
+    final = wgs84.GeoPoint(dmsp_data["GDLAT"][qualFlag][dmspTindex + 1], 
+                          dmsp_data["GLON"][qualFlag][dmspTindex + 1], 0, degrees=True)
+    
+    p_AB_N = init.delta_to(final) 
+    north, east, z = p_AB_N.pvector.ravel()
+    
+    
+    #use geometry to find components of dmsp measurement velocity
+    theta_for = np.arctan2(north, east)
+    alpha = np.arctan(abs(forward) / abs(left))
+    theta = theta_for + np.pi/2 - alpha
+    mag = np.hypot(forward, left)
 
-    #compare data
-    for sat in range(16,19):
-        
-        print(sat)
-        
-        #read data
-        sat_file = "dms_ut_201405%i_%i.002.p" % (day, sat)
-        dmsp_data = pickle.load( open(sat_file, "rb"))
-        
-        #quality flags for data
-        forQualIndex = dmsp_data["ION_V_FOR_FLAG"] == 1
-        leftQualIndex = dmsp_data["ION_V_LEFT_FLAG"] == 1
-        qualFlag = forQualIndex & leftQualIndex
-        
-        #set time index for dmsp data
-        startDay = datetime(2014, 5, day)
-        dayUnix = time.mktime(startDay.timetuple())
-        timeUnix = dayUnix + vel_data["time"][modTimeIndex]*3600
-        dmspTindex = nearest_index(timeUnix, dmsp_data["UT1_UNIX"][qualFlag])
-        
-        #read satellite data
-        satLat = dmsp_data["GDLAT"][qualFlag][dmspTindex]
-        satLon = dmsp_data["GLON"][qualFlag][dmspTindex]
-        forward = dmsp_data["ION_V_SAT_FOR"][qualFlag][dmspTindex]
-        left = dmsp_data["ION_V_SAT_LEFT"][qualFlag][dmspTindex]
-        
-        #create north/east vectors based on satellite direction
-        wgs84 = nv.FrameE(name='WGS84')
-        init = wgs84.GeoPoint(satLat, 
-                              satLon, 0, degrees=True)
-        
-        final = wgs84.GeoPoint(dmsp_data["GDLAT"][qualFlag][dmspTindex + 1], 
-                              dmsp_data["GLON"][qualFlag][dmspTindex + 1], 0, degrees=True)
-        
-        p_AB_N = init.delta_to(final) 
-        north, east, z = p_AB_N.pvector.ravel()
-        
-        
-        #use geometry to find components of dmsp measurement velocity
-        theta_for = np.arctan2(north, east)
-        alpha = np.arctan(abs(forward) / abs(left))
-        theta = theta_for + np.pi/2 - alpha
-        mag = np.hypot(forward, left)
+    #dmsp measurement components
+    dmsp_east = mag * np.cos(theta)
+    dmsp_north = mag * np.sin(theta)
     
-        #dmsp measurement components
-        dmsp_east = mag * np.cos(theta)
-        dmsp_north = mag * np.sin(theta)
-        
+
+    #interpolate model data between satellite coordinates
+    interp_mod_east = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
+                            vel_data["u3h0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
+    interp_mod_north = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
+                            vel_data["u1p0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
     
-        #interpolate model data between satellite coordinates
-        interp_mod_east = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
-                                vel_data["u3h0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
-        interp_mod_north = griddata((vel_data["lon0"].flatten(), vel_data["lat0"].flatten()), 
-                                vel_data["u1p0"][modTimeIndex].flatten(), (satLon, satLat), method = "linear")
-        
+
     
-        
-        #ax.quiver(dmsp_data["GLON"][dmspTindex], dmsp_data["GDLAT"][dmspTindex], 
-        #          interp_mod_x, interp_mod_y, color = colors[sat -15])
-        
+    #ax.quiver(dmsp_data["GLON"][dmspTindex], dmsp_data["GDLAT"][dmspTindex], 
+    #          interp_mod_x, interp_mod_y, color = colors[sat -15])
     
+
         
         
     

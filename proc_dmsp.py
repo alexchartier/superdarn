fb3dbaac3f584c9b457cb03ec0a2ade4cd944b07
Alex Chartier <alex.chartier@outlook.com>
2021-07-30 10:56:56 -0400

    forgot to put this one in

--- proc_dmsp.py
+++ proc_dmsp.py
@@ -1,18 +1,17 @@
 import numpy as np
 import h5py
-import pickle
-import time
-import matplotlib.pyplot as plt
-import os
-from os import path
+import datetime as dt
 
 def main(
-    fn = 'data/dmsp/dms_ut_20140523_15.002.hdf5',
+    fn = 'data/dmsp/dms_ut_20140523_16.002.hdf5',
 ):
-    flist = get_file_list(data_dir)
-    for fn in flist:
-        raw_dmsp_data = load_data(fn)
-        processed_dmsp_data = proc_data(raw_dmsp_data)
+    #flist = get_file_list(data_dir)
+    #for fn in flist:
+    raw_dmsp_data = load_data(fn)
+    processed_dmsp_data = proc_data(raw_dmsp_data)
+
+def load(fn):
+    return proc_data(load_data(fn))
 
 
 def load_data(fn):
@@ -25,7 +24,7 @@ def load_data(fn):
     vars = {}
     out = {}
     for ind, prm in enumerate(parameters):
-        str = prm[0].decode('utf-8')
+        str = prm[0].decode('utf-8')  # removing those annoying b' prefixes
         vars[str] = ind 
         out[str] = []
     
@@ -40,17 +39,27 @@ def load_data(fn):
 
 def proc_data(dmsp_data):
 
-    goodind = np.logical_and(dmsp_data['RPA_FLAG_UT'] < 2, dmsp_data['IDM_FLAG_UT'] < 2)
+    # remove flagged bad data
+
+    forQualIndex = dmsp_data["ION_V_FOR_FLAG"] == 1
+    leftQualIndex = dmsp_data["ION_V_LEFT_FLAG"] == 1
+    qualFlag = forQualIndex & leftQualIndex
+
+    #goodInd = np.logical_and(dmsp_data['RPA_FLAG_UT'] < 2, dmsp_data['IDM_FLAG_UT'] < 2)  # NOTE: This is only for F15
     for k, v in dmsp_data.items():
-        dmsp_data[k] = v[goodind]
+        dmsp_data[k] = v[qualFlag]
+    dmsp_data['Time'] = []
 
-    out = {}
-    vars = 'Time', 'Alt', 'Lat', 'Lon', 'Vel_perp'
-    for v in vars:
-        out[v] = []
+    # Store values
     for ind, yr in enumerate(dmsp_data['YEAR']):
-        time = dt.datetime(yr, dmsp_data['MONTH'], dmsp_data['DAY'], dmsp_data['HOUR'], dmsp_data['MIN'], dmsp_data['SEC'])
+        time = dt.datetime(
+            yr, dmsp_data['MONTH'][ind], dmsp_data['DAY'][ind], dmsp_data['HOUR'][ind], 
+            dmsp_data['MIN'][ind], dmsp_data['SEC'][ind],
+        )
+        dmsp_data['Time'].append(time)
 
+    return dmsp_data
+    
 
 if __name__ == '__main__':
     main() 

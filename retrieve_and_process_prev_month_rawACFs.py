c885b044757f44a280c10abbc6fdabccbcc36c35
JWiker <jordan.wiker@gmail.com>
2021-09-29 08:20:47 -0400

    Refactor retrieve and process script

--- retrieve_and_process_prev_month_rawACFs.py
+++ retrieve_and_process_prev_month_rawACFs.py
@@ -6,7 +6,6 @@ import datetime
 import raw_to_nc
 
 EMAIL_ADDRESSES = 'jordan.wiker@jhuapl.edu'#,Alex.Chartier@jhuapl.edu'
-MAKE_FIT_VERSION = 2.5
 DOWNLOAD_RAWACFS = False
 MAX_NUM_RSYNC_TRIES = 3
 
@@ -15,17 +14,15 @@ def main():
     dirs  = {
         'basServer': 'apl@bslsuperdarnb.nerc-bas.ac.uk',
         'basRawacf': 'apl@bslsuperdarnb.nerc-bas.ac.uk:/sddata/raw/%Y/%m/',        
-        'rawacf': '/project/superdarn/data/rawacf/%Y/%m/',
+        'rawacf': '/homes/superdarn/20140424/',
         'fitacf': '/project/superdarn/data/fitacf/%Y/%m/',
         'netcdf': '/project/superdarn/data/netcdf/%Y/%m/',
         'log': '/homes/superdarn/logs/'
     }
         
-    # if not "RSTPATH" in os.environ:
-    #     os.environ['RSTPATH'] = '/project/superdarn/software/rst'
-
     startDate, endDate = get_first_and_last_days_of_prev_month()
-    download_rawacfs(dirs, startDate)
+    if DOWNLOAD_RAWACFS:
+        download_rawacfs(dirs, startDate)
     convert_rawacf_to_fitacf_and_netcdf(startDate, endDate, dirs)
 
 def download_rawacfs(dirs, startDate):
@@ -39,55 +36,49 @@ def download_rawacfs(dirs, startDate):
     fileNameDateString = startDate.strftime('%Y_%m')
     rsyncLogFilename = logsDir + '/BAS_rsync_logs/{datePrefix}_BAS_rsync.out'.format(datePrefix = fileNameDateString)
 
-    if DOWNLOAD_RAWACFS:
-        # Save a list of all rawACF files on BAS for the given month and store it in the netcdf directory
-        os.system('ssh {bas} ls /sddata/raw/{date} > {ncDir}/bas_rawacfs_{dateSuffix}.txt'.format(bas = basServer, date = dateString, ncDir = netcdfDir, dateSuffix = fileNameDateString))
-        #TODO: replace os.system with subprocess.call
-        # subprocess.call(('ssh {bas} ls /sddata/raw/{date} > '
-        #                 '{ncDir}/bas_rawacfs_{dateSuffix}.txt'.
-        #                 format(bas = basServer, date = dateString, ncDir = netcdfDir, dateSuffix = fileNameDateString)))
-
-        # Remove the first line (hashes filename)
-        os.system('sed -i \'1d\' {ncDir}/bas_rawacfs_{dateSuffix}.txt'.format(ncDir = netcdfDir, dateSuffix = fileNameDateString))
+    # Save a list of all rawACF files on BAS for the given month and store it in the netcdf directory
+    os.system('ssh {bas} ls /sddata/raw/{date} > {ncDir}/bas_rawacfs_{dateSuffix}.txt'.format(bas = basServer, date = dateString, ncDir = netcdfDir, dateSuffix = fileNameDateString))
+    # Remove the first line (hashes filename)
+    os.system('sed -i \'1d\' {ncDir}/bas_rawacfs_{dateSuffix}.txt'.format(ncDir = netcdfDir, dateSuffix = fileNameDateString))
+    
+    # Copy all rawACF files from BAS for the given month
+    # rawacfDir = '/project/superdarn/data/rawacf/%s' % dateString
+    # os.makedirs(rawacfDir, exist_ok=True)
+    
+    numTries = 0
+    rsyncSuccess = False
+    while numTries < MAX_NUM_RSYNC_TRIES:
+        os.system('nohup rsync -rv {basRaw} {rawDir} >& {logFile}'.format(basRaw = basRawacf, rawDir = rawacfDir, logfile = rsyncLogFilename))
+    
+        # Check that all files were copied
+        os.system('ls {rawDir} > {ncDir}/bas_rawacfs_copied_{dateSuffix}.txt'.format(rawDir = rawacfDir, ncDir = netcdfDir, dateSuffix = fileNameDateString))
+    
+        basList = "{ncDir}/bas_rawacfs_{dateSuffix}.txt".format(ncDir = netcdfDir, dateSuffix = fileNameDateString)
+        aplList = "{ncDir}/bas_rawacfs_copied_{dateSuffix}.txt".format(ncDir = netcdfDir, dateSuffix = fileNameDateString)
+    
+        # Compare the list of files on BAS to the list of copied files at APL
+        result = filecmp.cmp(basList, aplList, shallow=False)
+    
+        # If the BAS file list matches the APL file list, the rsync succeeded and the rawACFs are
+        # ready to be processed
+        if result == 0:
+            rsyncSuccess = True
+            break
         
-        # Copy all rawACF files from BAS for the given month
-        # rawacfDir = '/project/superdarn/data/rawacf/%s' % dateString
-        # os.makedirs(rawacfDir, exist_ok=True)
-        
-        numTries = 0
-        rsyncSuccess = False
-        while numTries < MAX_NUM_RSYNC_TRIES:
-            os.system('nohup rsync -rv {basRaw} {rawDir} >& {logFile}'.format(basRaw = basRawacf, rawDir = rawacfDir, logfile = rsyncLogFilename))
-        
-            # Check that all files were copied
-            os.system('ls {rawDir} > {ncDir}/bas_rawacfs_copied_{dateSuffix}.txt'.format(rawDir = rawacfDir, ncDir = netcdfDir, dateSuffix = fileNameDateString))
-        
-            basList = "{ncDir}/bas_rawacfs_{dateSuffix}.txt".format(ncDir = netcdfDir, dateSuffix = fileNameDateString)
-            aplList = "{ncDir}/bas_rawacfs_copied_{dateSuffix}.txt".format(ncDir = netcdfDir, dateSuffix = fileNameDateString)
-        
-            # Compare the list of files on BAS to the list of copied files at APL
-            result = filecmp.cmp(basList, aplList, shallow=False)
-        
-            # If the BAS file list matches the APL file list, the rsync succeeded and the rawACFs are
-            # ready to be processed
-            if result == 0:
-                rsyncSuccess = True
-                break
-            
-            numTries += 1
-        
-        # If the rsync did not succeed, send a notification email and end the script
-        if not rsyncSuccess:
-            emailSubject = '"Unsuccessful attempt to copy {date} BAS rawACF Data"'.format(date = dateString)
-            emailBody    = '"Tried to copy {date} rawACFs from BAS {num} times, but did not succeed. \nSee {logfile} for more details."'.format(date = dateString, num = MAX_NUM_RSYNC_TRIES, logfile = rsyncLogFilename)
-            send_email(emailSubject, emailBody, EMAIL_ADDRESSES)
-            sys.exit('{message}'.format(message = emailBody))
-            
-        # Send a notification email saying the rsync succeeded
-        emailSubject = '"{date} BAS rawACF Data Successfully Copied"'.format(date = dateString)
-        emailBody    = '"{date} rawACF files from BAS have been copied. Starting conversion to netCDF."'.format(date = dateString)  
-        os.system('rm {ncDir}/bas_rawacfs_copied_{dateSuffix}.txt'.format(ncDir = netcdfDir, dateSuffix = fileNameDateString))
+        numTries += 1
+    
+    # If the rsync did not succeed, send a notification email and end the script
+    if not rsyncSuccess:
+        emailSubject = '"Unsuccessful attempt to copy {date} BAS rawACF Data"'.format(date = dateString)
+        emailBody    = '"Tried to copy {date} rawACFs from BAS {num} times, but did not succeed. \nSee {logfile} for more details."'.format(date = dateString, num = MAX_NUM_RSYNC_TRIES, logfile = rsyncLogFilename)
         send_email(emailSubject, emailBody, EMAIL_ADDRESSES)
+        sys.exit('{message}'.format(message = emailBody))
+        
+    # Send a notification email saying the rsync succeeded
+    emailSubject = '"{date} BAS rawACF Data Successfully Copied"'.format(date = dateString)
+    emailBody    = '"{date} rawACF files from BAS have been copied. Starting conversion to netCDF."'.format(date = dateString)  
+    os.system('rm {ncDir}/bas_rawacfs_copied_{dateSuffix}.txt'.format(ncDir = netcdfDir, dateSuffix = fileNameDateString))
+    send_email(emailSubject, emailBody, EMAIL_ADDRESSES)
     
 def convert_rawacf_to_fitacf_and_netcdf(startDate, endDate, dirs):
 
@@ -95,7 +86,7 @@ def convert_rawacf_to_fitacf_and_netcdf(startDate, endDate, dirs):
     fitacfDir = startDate.strftime(dirs['fitacf'])
     netcdfDir = startDate.strftime(dirs['netcdf'])
 
-    raw_to_nc.main(startDate, endDate, rawacfDir,fitacfDir,netcdfDir,MAKE_FIT_VERSION)
+    raw_to_nc.main(startDate, endDate, rawacfDir,fitacfDir,netcdfDir)
 
     dateString = startDate.strftime('%Y/%m')
 
@@ -109,8 +100,8 @@ def get_first_and_last_days_of_prev_month():
         firstDay = lastDay.replace(day=1)
     
 
-        firstDay = datetime.datetime(2021,8,5,0,0)
-        lastDay = datetime.datetime(2021,8,5,0,0)
+        firstDay = datetime.datetime(2014,4,24,0,0)
+        lastDay = datetime.datetime(2014,4,24,0,0)
         return firstDay, lastDay 
 
 def send_email(subject, body, addresses):

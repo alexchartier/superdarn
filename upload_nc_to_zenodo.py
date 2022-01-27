3fd542cd44d0995503aad102be56b0dee3cf78f2
Jordan Wiker <jordan.wiker@jhuapl.edu>
2022-01-27 11:24:29 -0500

    Updated zenodo upload script

--- upload_nc_to_zenodo.py
+++ upload_nc_to_zenodo.py
@@ -11,12 +11,14 @@ Terms:
 """  
 import sys, os
 # TODO: Remove this once requests is installed for python3
-sys.path.append('/usr/lib/python2.7/site-packages/')
+# sys.path.append('/usr/lib/python2.7/site-packages/')
 import requests
 import datetime as dt
 from dateutil.relativedelta import relativedelta
 import argparse
 import helper
+import json
+import glob
     
 __author__ = "Jordan Wiker"
 __copyright__ = "Copyright 2022, JHUAPL"
@@ -25,91 +27,185 @@ __maintainer__ = "Jordan Wiker"
 __email__ = "jordan.wiker@jhuapl.edu"
 __status__ = "Development"
 
+# Zenodo upload metadata
+wikerORCID = '0000-0003-4707-5539'
+chartierORCID = '0000-0002-4215-031X'
+aplAffiliation = 'JHU/APL'
+keywords = 'SuperDARN, ionosphere, magnetosphere, convection, rst, fitacf, netcdf, aeronomy, cedar, gem, radar, OTHR'
+
+SANDBOX = True
+
 # TODO: Add non-sandbox Zenodo token, and confirm deposit:write is enabled
-ZENODO_TOKEN = ''
+ZENODO_TOKEN = '9FWXXWi1NYeEo6c7zarVtOEOzUkPwiwgVNJ6FD2Wyzecf3PNrs1HKKnrDjYS'
 ZENODO_SANDBOX_TOKEN = '9FWXXWi1NYeEo6c7zarVtOEOzUkPwiwgVNJ6FD2Wyzecf3PNrs1HKKnrDjYS'
 
-def main(
-    date = dt.datetime(2020, 7, 1),
-    sandbox = True
-):
-    accessToken = ZENODO_SANDBOX_TOKEN if sandbox else ZENODO_TOKEN
+def main(date):
+    accessToken = ZENODO_SANDBOX_TOKEN if SANDBOX else ZENODO_TOKEN
     uploadDir = date.strftime(helper.NETCDF_DIR_FMT)
-    print(date)
-    print(uploadDir)
-    print(sandbox)
+    upload_to_zenodo(accessToken, uploadDir)
     
 
-def _build_arg_parser(Parser, *args):
-    scriptname = os.path.basename(sys.argv[0])
+def upload_to_zenodo(accessToken, uploadDir):
+    # TODO: Remove this dir
+    uploadDir = '/Users/wikerjr1/Desktop/zenodotest/'
+    fileList = glob.glob(os.path.join(uploadDir, '*.nc'))
 
-    formatter = argparse.RawDescriptionHelpFormatter(scriptname)
-    width = formatter._width
+    headers = {"Content-Type": "application/json"}
 
-    title = "zenodo_uploader"
-    copyright = "Copyright (c) 2022 JHU/APL"
-    shortdesc = "Upload SuperDARN netCDF files to Zenodo"
-    desc = "\n".join(
-        (
-            "*" * width,
-            "*{0:^{1}}*".format(title, width - 2),
-            "*{0:^{1}}*".format(copyright, width - 2),
-            "*{0:^{1}}*".format("", width - 2),
-            "*{0:^{1}}*".format(shortdesc, width - 2),
-            "*" * width,
-        )
-    )
+    params = {'access_token': accessToken}
+    r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions',
+                   params=params,
+                   json={},
+                   # Headers are not necessary here since "requests" automatically
+                   # adds "Content-Type: application/json", because we're using
+                   # the "json=" keyword argument
+                   # headers=headers,
+                   headers=headers)
 
-    usage = (
-        "%s [sandbox] [-d YYYY-m-d]" % scriptname
-    )
+    bucket_url = r.json()["links"]["bucket"]
+    deposition_id = r.json()['id']
 
-    # parse options
-    parser = Parser(
-        description=desc,
-        usage=usage,
-        prefix_chars="-+",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
+    # The target URL is a combination of the bucket link with the desired filename
+    # seperated by a slash.
+    for file in fileList:
+        filename = file.split('/')[-1]
+        with open(file, "rb") as fp:
+            r = requests.put(
+                "%s/%s" % (bucket_url, filename),
+                data=fp,
+                params=params,
+            )
+    
+    # data = {
+    #     'metadata': {
+    #         'title': 'My first upload',
+    #         'upload_type': 'dataset',
+    #         'description': 'This is my first upload',
+    #         'creators': [{'name': 'Wiker, Jordan','affiliation': 'JHUAPL'}],
+    #         'communities': [{"id": "superdarn"}]
+    #         }
+    #     }
 
-    parser.add_argument(
-        "--sandbox",
-        action="store_true",
-        help="""Upload to the APL Sandbox Zenodo account""",
-    )
+    # r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id, params={'access_token': accessToken}, data=json.dumps(data), headers=headers)
+    
+    
+    data = {
+        'metadata': {
+            'title': 'Test Upload',
+            'upload_type': 'dataset',
+            'description': 'Test upload',
+            'creators': [
+                {
+                    'orcid': chartierORCID, 
+                    'affiliation': aplAffiliation, 
+                    'name': 'Chartier, Alex T.'
+                }, 
+                {
+                    'orcid': chartierORCID,
+                    'affiliation': aplAffiliation,
+                    'name': 'Wiker, Jordan'
+                }
+            ],
+            'keywords': [
+                'SuperDARN, ionosphere, magnetosphere, convection, rst, fitacf, cfit, aeronomy, cedar, gem, radar'
+            ],
+            "communities": [
+                {
+                    "identifier": "spacephysics"
+                } 
+                # {
+                #     "id": "superdarn"
+                # }
+            ], 
+        }
+    }
 
-    parser.add_argument(
-        "-d",
-        "--date",
-        dest="date",
-        default=None,
-        help="""Month to upload - format: 'YYYY-m-d'""",
-    )
+    r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
+    params={'access_token': accessToken}, data=json.dumps(data), headers=headers)
 
-    return parser
+
+# def _build_arg_parser(Parser, *args):
+#     scriptname = os.path.basename(sys.argv[0])
+
+#     formatter = argparse.RawDescriptionHelpFormatter(scriptname)
+#     width = formatter._width
+
+#     title = "zenodo_uploader"
+#     copyright = "Copyright (c) 2022 JHU/APL"
+#     shortdesc = "Upload SuperDARN netCDF files to Zenodo"
+#     desc = "\n".join(
+#         (
+#             "*" * width,
+#             "*{0:^{1}}*".format(title, width - 2),
+#             "*{0:^{1}}*".format(copyright, width - 2),
+#             "*{0:^{1}}*".format("", width - 2),
+#             "*{0:^{1}}*".format(shortdesc, width - 2),
+#             "*" * width,
+#         )
+#     )
+
+#     usage = (
+#         "%s [sandbox] [-d YYYY-m-d]" % scriptname
+#     )
+
+#     # parse options
+#     parser = Parser(
+#         description=desc,
+#         usage=usage,
+#         prefix_chars="-+",
+#         formatter_class=argparse.RawDescriptionHelpFormatter,
+#     )
+
+#     parser.add_argument(
+#         "--sandbox",
+#         action="store_true",
+#         help="""Upload to the APL Sandbox Zenodo account""",
+#     )
+
+#     parser.add_argument(
+#         "-d",
+#         "--date",
+#         dest="date",
+#         default=None,
+#         help="""Month to upload - format: 'YYYY-m-d'""",
+#     )
+
+#     return parser
 
 
 if __name__ == '__main__':
-    parser = _build_arg_parser(argparse.ArgumentParser)
-    args = parser.parse_args()
+    args = sys.argv
     
-    sandbox = False
-    today = dt.datetime.now()
-    date = today - relativedelta(months=1)
-
-    if args.sandbox:
-        sandbox = True
-
-    if args.date:
-        components = args.date.split('-')
-        year = int(components[0])
-        month = int(components[1])
-        day = int(components[2])
-        date = dt.datetime(year, month, day)
+    if len(args) < 2:
+        # If no date was passed in, process the previous month
+        today = dt.datetime.now()
+        date = today - relativedelta(months=1)
     else:
-        print('No date provided - uploading {0} netCDFs to Zenodo'.format(date.strftime("%Y/%m")))
+        date = args[1]
     
-    main(date, sandbox)
+    main(date)
+
+# if __name__ == '__main__':
+#     parser = _build_arg_parser(argparse.ArgumentParser)
+#     args = parser.parse_args()
+    
+#     sandbox = False
+#     today = dt.datetime.now()
+#     date = today - relativedelta(months=1)
+
+#     if args.sandbox:
+#         sandbox = True
+
+#     if args.date:
+#         components = args.date.split('-')
+#         year = int(components[0])
+#         month = int(components[1])
+#         day = int(components[2])
+#         date = dt.datetime(year, month, day)
+#     else:
+#         print('No date provided - uploading {0} netCDFs to Zenodo'.format(date.strftime("%Y/%m")))
+    
+#     main(date, sandbox)
 
 
 

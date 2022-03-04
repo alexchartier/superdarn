d347a361110640e3ed5c0846ef800cbca72ade4f
Jordan Wiker <jordan.wiker@jhuapl.edu>
2022-03-04 16:18:35 -0500

    Added scripts to pull and process fitACF files directly from globus

--- process_all_data.py
+++ process_all_data.py
@@ -18,7 +18,6 @@ import upload_nc_to_zenodo
 
 START_DATE = dt.datetime(2017, 6, 1)
 END_DATE = dt.datetime(2017, 6, 1)
-# END_DATE = dt.datetime(2006,8, 1) # Earlier than this there's a mix of dat and raw, or only dat
 
 LOG_DIR = '{0}rawACF_to_netCDF_logs'.format(helper.LOG_DIR)
 
@@ -26,20 +25,14 @@ def main():
     date = START_DATE
     while date >= END_DATE:
         logDir = '{0}/{1}'.format(LOG_DIR, date.strftime('%Y'))
-        logFile = '{0}/download_and_process_{1}.log'.format(logDir, date.strftime('%Y%m'))
         os.makedirs(logDir, exist_ok=True)
 
         emailSubject = '"PROCESS ALL STARTING"'
         emailBody    = 'Starting to download and process {0} data'.format(date.strftime('%Y%m'))
         helper.send_email(emailSubject, emailBody)
 
-        original_stdout = sys.stdout
-#        f = open(logFile, 'w')
-#        sys.stdout = f
-
         download_and_process_rawacfs.main(date)
         upload_nc_to_zenodo.main(date)        
-        sys.stdout = original_stdout 
 
         emailSubject = '"PROCESS ALL COMPLETE"'
         emailBody    = 'Finished downloading and processing {0} data'.format(date.strftime('%Y%m'))

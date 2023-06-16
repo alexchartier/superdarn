a141205044d890818ef947e1ee5909a00ffa304d
wikerjr1 <jordan.wiker@jhuapl.edu>
2023-06-16 16:02:42 -0400

    Started globus download functions

--- process_missing_files.py
+++ process_missing_files.py
@@ -8,16 +8,16 @@ import json
 
 VALID_FILE_TYPES = ['rawacf', 'fitacf', 'fit_nc', 'meteorwind', 'meteorwind_nc', 'grid', 'grid_nc']
 
+GLOBUS_SOURCE_FILES = {'rawacf':'raw', 
+                       'fitacf':'fitacf_30', 
+                       'fit_nc':'despeck_fitacf_30', 
+                       'meteorwind':'fitacf_30', 
+                       'meteorwind_nc':'meteorwind', 
+                       'grid':'fitacf_30', 
+                       'grid_nc':'grid'}
 
-def get_local_file_list(date, file_types=None):
+def get_local_file_list(file_types=None):
     file_list = {}
-    rawacf_dir = date.strftime(helper.RAWACF_DIR_FMT)
-    fitacf_dir = date.strftime(helper.FITACF_DIR_FMT)
-    fit_nc_dir = date.strftime(helper.FIT_NC_DIR_FMT)
-    meteorwind_dir = date.strftime(helper.METEORWIND_DIR_FMT)
-    meteorwind_nc_dir = date.strftime(helper.METEORWIND_NC_DIR_FMT)
-    grid_dir = date.strftime(helper.GRID_DIR_FMT)
-    grid_nc_dir = date.strftime(helper.GRID_NC_DIR_FMT)
 
     day_string = date.strftime('%Y%m%d')
     day_string_two = date.strftime('%Y%b%d')
@@ -76,17 +76,65 @@ def get_local_file_list(date, file_types=None):
 
     return file_list
 
-
-def get_globus_file_list(date):
+def get_globus_file_list():
     day = date.strftime('%Y%m%d')
     with open(f'{helper.GLOBUS_FILE_LIST_DIR}/globus_data_inventory.json') as f:
         remote_data = json.load(f)
     return remote_data.get(day, [])
 
+def produce_rawacf(files):
 
-def produce_missing_files(missing_files):
     return
 
+def produce_fitacf(files):
+    download_files('fitacf_30', files, fitacf_dir)
+
+def produce_fit_nc(files):
+    return
+
+def produce_meteorwind(files):
+    return
+
+def produce_meteorwind_nc(files):
+    return
+
+def produce_grid(files):
+    return
+
+def produce_grid_nc(files):
+    return
+
+DATA_PRODUCTION_FUNCTIONS = {
+    'rawacf': produce_rawacf,
+    'fitacf': produce_fitacf,
+    'fit_nc': produce_fit_nc,
+    'meteorwind': produce_meteorwind,
+    'meteorwind_nc': produce_meteorwind_nc,
+    'grid': produce_grid,
+    'grid_nc': produce_grid_nc
+}
+
+# Produce all missing files for the given day
+def produce_missing_files(missing_files):
+    for file_type_to_produce, files in missing_files.items():
+        if files:
+            production_function = DATA_PRODUCTION_FUNCTIONS.get(file_type_to_produce)
+            if production_function:
+                breakpoint()
+                production_function(files)
+            else:
+                print(f'No data production function found for file type: {file_type_to_produce}')
+            
+def download_files(globus_file_type, stations, destination_dir):
+    for station in stations:
+        pattern = date.strftime('%Y%m%d') + '*' + station
+
+        breakpoint()
+
+        # Initiate the Globus -> APL transfer
+        subprocess.run(['nohup', '/software/python-3.11.4/bin/python3', 
+                        '/homes/superdarn/superdarn/globus/sync_radar_data_globus.py',
+                        '-y', str(date.year), '-m', str(date.month), '-t', globus_file_type, '-p', pattern, destination_dir])
 
 def main(start_date, end_date, file_types):
     if file_types is None:
@@ -106,13 +154,24 @@ def main(start_date, end_date, file_types):
                 print(f'  {file_type}')
             print('\nExample usage: python3 process_missing_files.py -t fitacf meteorwind_nc\n')
             return
+        
+    global rawacf_dir, fitacf_dir, fit_nc_dir, meteorwind_dir, meteorwind_nc_dir, grid_dir, grid_nc_dir, date
 
     date = start_date
     while date >= end_date:
         print(f'Checking files for {date.strftime("%Y-%m-%d")}...')
 
-        globus_files = get_globus_file_list(date)
-        local_files = get_local_file_list(date, file_types)
+        # Populate the directory paths for the current date
+        rawacf_dir = date.strftime(helper.RAWACF_DIR_FMT)
+        fitacf_dir = date.strftime(helper.FITACF_DIR_FMT)
+        fit_nc_dir = date.strftime(helper.FIT_NC_DIR_FMT)
+        meteorwind_dir = date.strftime(helper.METEORWIND_DIR_FMT)
+        meteorwind_nc_dir = date.strftime(helper.METEORWIND_NC_DIR_FMT)
+        grid_dir = date.strftime(helper.GRID_DIR_FMT)
+        grid_nc_dir = date.strftime(helper.GRID_NC_DIR_FMT)
+
+        globus_files = get_globus_file_list()
+        local_files = get_local_file_list(file_types)
         missing_files = {}
 
         for file_type, files in local_files.items():
@@ -126,11 +185,12 @@ def main(start_date, end_date, file_types):
                 if not file_exists:
                     missing_files[file_type].append(globus_file)
 
-        if missing_files:
-            print(f'Missing files for {date.strftime("%Y-%m-%d")} on the local system:')
-            for file_type, files in missing_files.items():
-                if files:
-                    print(f'{file_type}: {files}')
+        # TODO: Remove this block
+        # if missing_files:
+        #     print(f'Missing files for {date.strftime("%Y-%m-%d")} on the local system:')
+        #     for file_type, files in missing_files.items():
+        #         if files:
+        #             print(f'{file_type}: {files}')
 
         produce_missing_files(missing_files)
 

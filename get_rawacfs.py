ee8c0c88d0d3b749b8af986f122736572f986daf
Jordan Wiker <jordan.wiker@jhuapl.edu>
2023-09-25 10:34:50 -0400

    Refactor and update comments

--- get_rawacfs.py
+++ get_rawacfs.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 # coding: utf-8
 """
-Downloads and combines rawACF files for the specified date from the specified source
+Downloads and combines rawACF files for the specified date from the specified source.
 """
-import sys, os
+
+import sys
+import os
 import datetime
 import socket
 import time
-from dateutil.relativedelta import relativedelta
 import helper
 import subprocess
 
-DELAY = 1800 # 30 minutes
-RETRY = 17   # Try to connect every 30 minutes for a day
-TIMEOUT = 10 # seconds
+DELAY = 1800  # 30 minutes
+RETRY = 17    # Try to connect every 30 minutes for a day
+TIMEOUT = 10  # seconds
 MAX_NUM_RSYNC_TRIES = 3
 
 # Global date variable
 date = None
 
 def main(dateString, dataSource=None):
-    global date  # Access the global date variable
+    """
+    Downloads rawACF files for the specified date from the specified source and combines them.
+
+    Args:
+        dateString (str): The date in 'YYYYMMDD' format.
+        dataSource (str, optional): The data source ('bas' or 'globus'). Defaults to 'bas'.
+    """
+    global date
     date = datetime.datetime.strptime(dateString, '%Y%m%d')
     if dataSource is None:
         dataSource = 'bas'
@@ -30,8 +38,13 @@ def main(dateString, dataSource=None):
     download_source_files(dataSource)
     combine_source_files()
 
-
 def download_source_files(dataSource):
+    """
+    Downloads rawACF files from the specified data source.
+
+    Args:
+        dataSource (str): The data source ('bas' or 'globus').
+    """
     rawDir = date.strftime(helper.RAWACF_DIR_FMT)
     os.makedirs(rawDir, exist_ok=True)
 
@@ -40,60 +53,74 @@ def download_source_files(dataSource):
     elif dataSource == 'globus':
         download_files_from_globus(rawDir)
     else:
-        print('ERROR: Specified rawACF source is invalid {}. Valid options are: \'bas\' or \'globus\''.format(dataSource))
-
+        print(f'ERROR: Specified rawACF source is invalid {dataSource}. Valid options are: \'bas\' or \'globus\'')
 
 def download_files_from_globus(rawDir):
+    """
+    Downloads rawACF files from Globus.
+
+    Args:
+        rawDir (str): The directory to save the downloaded files.
+    """
     # Start Globus Connect Personal and establish connection
     # Also allow access to /project/superdarn/data/
-    subprocess.call('{0} -start -restrict-paths \'rw~/,rw/project/superdarn/data\' &'.format(helper.GLOBUS_PATH), shell=True)
+    subprocess.call(f'{helper.GLOBUS_PATH} -start -restrict-paths \'rw~/,rw/project/superdarn/data\' &', shell=True)
 
     # Initiate the transfer from Globus to APL
-    subprocess.call('nohup /project/superdarn/software/python-3.8.1/bin/python3 /homes/superdarn/superdarn/globus/sync_radar_data_globus.py -y {0} -m {1} -t {2} {3}'.format(date.year, date.month, 'raw', rawDir), shell=True)
+    subprocess.call(f'nohup /project/superdarn/software/python-3.8.1/bin/python3 /homes/superdarn/superdarn/globus/sync_radar_data_globus.py -y {date.year} -m {date.month} -t raw {rawDir}', shell=True)
 
     # Stop Globus Connect Personal
-    subprocess.call('{0} -stop'.format(helper.GLOBUS_PATH), shell=True)
+    subprocess.call(f'{helper.GLOBUS_PATH} -stop', shell=True)
 
-    # emailSubject = '"{0} rawACF Data Successfully Downloaded from Globus"'.format(date.strftime('%Y/%m'))
-    # emailBody    = '"{0} rawACF source files have been downloaded. Starting conversion to fitACF and netCDF."'.format(date.strftime('%Y/%m'))
+    # emailSubject = f'"{date.strftime("%Y/%m")} rawACF Data Successfully Downloaded from Globus"'
+    # emailBody = f'"{date.strftime("%Y/%m")} rawACF source files have been downloaded. Starting conversion to fitACF and netCDF."'
     # helper.send_email(emailSubject, emailBody)
 
-
 def download_files_from_bas(rawDir):
+    """
+    Downloads rawACF files from BAS.
 
-    basRawDir = date.strftime(helper.BAS_RAWACF_DIR_FMT )
+    Args:
+        rawDir (str): The directory to save the downloaded files.
+    """
+    basRawDir = date.strftime(helper.BAS_RAWACF_DIR_FMT)
 
     # Make sure the BAS server is reachable
     if not BASServerConnected():
-        # Send email if BAS couldn't be reached  
+        # Send email if BAS couldn't be reached
         emailSubject = '"Unable to reach BAS"'
-        emailBody    = 'Unable to reach BAS after trying for {hours} hours.'.format(hours = RETRY * DELAY / 3600)
+        emailBody = 'Unable to reach BAS after trying for {hours} hours.'.format(hours=RETRY * DELAY / 3600)
         helper.send_email(emailSubject, emailBody)
-        sys.exit('{message}'.format(message = emailBody))
+        sys.exit('{message}'.format(message=emailBody))
 
     dateString = date.strftime('%Y-%m-%d')
-    print('Downloading {} rawACFs from BAS'.format(dateString))
+    print(f'Downloading {dateString} rawACFs from BAS')
     rsyncLogDir = os.path.join(helper.LOG_DIR, 'BAS_rsync_logs', date.strftime('%Y'))
     os.makedirs(rsyncLogDir, exist_ok=True)
     rsyncLogFilename = f'BAS_rsync_{dateString}.out'
     fullLogFilename = os.path.join(rsyncLogDir, rsyncLogFilename)
-    rsyncCommand = 'nohup rsync -rv apl@{0}:{1} {2} >& {3}'.format(helper.BAS_SERVER, basRawDir, rawDir, fullLogFilename)
+    rsyncCommand = f'nohup rsync -rv apl@{helper.BAS_SERVER}:{basRawDir} {rawDir} >& {fullLogFilename}'
     rsyncProcess = subprocess.Popen(rsyncCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     rsyncExitCode = rsyncProcess.wait()
 
     if rsyncExitCode != 0:
         # Send an email and end the script if rsync didn't succeed
-        emailSubject = '"Unsuccessful attempt to copy {0} BAS rawACF data"'.format(dateString)
-        emailBody    = '"Failed to copy {0} rawACFs from BAS with exit code {1}. \nSee {2} for more details."'.format(dateString, rsyncExitCode, rsyncLogFilename)
+        emailSubject = f'"Unsuccessful attempt to copy {dateString} BAS rawACF data"'
+        emailBody = f'"Failed to copy {dateString} rawACFs from BAS with exit code {rsyncExitCode}. \nSee {fullLogFilename} for more details."'
         helper.send_email(emailSubject, emailBody)
-        sys.exit('{message}'.format(message = emailBody))
-
+        sys.exit('{message}'.format(message=emailBody))
 
 def combine_source_files():
     # TODO
     return
 
 def BASServerConnected():
+    """
+    Checks if the BAS server is reachable.
+
+    Returns:
+        bool: True if the server is reachable, False otherwise.
+    """
     BASup = False
     for i in range(RETRY):
         if isOpen(helper.BAS_SERVER, 22):
@@ -103,8 +130,17 @@ def BASServerConnected():
             time.sleep(DELAY)
     return BASup
 
-
 def isOpen(server, port):
+    """
+    Checks if a server is reachable on the specified port.
+
+    Args:
+        server (str): The server hostname or IP address.
+        port (int): The port number to check.
+
+    Returns:
+        bool: True if the server is reachable on the port, False otherwise.
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(TIMEOUT)
     try:
@@ -115,7 +151,6 @@ def isOpen(server, port):
         return False
     finally:
         s.close()
-
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -131,6 +166,6 @@ if __name__ == '__main__':
     # Check if the day argument is in the correct format
     if not dateString.isdigit() or len(dateString) != 8:
         print("Date argument must be in 'YYYYMMDD' format.")
-        sys.exit(1)    
+        sys.exit(1)
 
     main(dateString, dataSource)

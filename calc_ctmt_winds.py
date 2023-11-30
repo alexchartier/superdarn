d2d58b467d00167c739c315945e12261bad9912d
Alex Chartier <alex.chartier@outlook.com>
2023-11-30 11:53:18 -0500

    2d plotting

--- calc_ctmt_winds.py
+++ calc_ctmt_winds.py
@@ -1,40 +1,164 @@
 """
-Plot the CTMT - Climatological Tidal Model of the Thermosphere
+Calculate winds from the CTMT - Climatological Tidal Model of the Thermosphere
 Aim to do SuperDARN winds analysis against it
 
 """
 import nc_utils   # available from github.com/alexchartier/nc_utils
 import numpy as np
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
 
-# diurnal and semidiurnal tidal filenames
-in_fn_sd = '~/Downloads/ctmt_semidiurnal_2002_2008.nc'
-in_fn_d = '~/Downloads/ctmt_diurnal_2002_2008.nc'
+from cartopy import config
+import cartopy.crs as ccrs
 
-# Location
-lat = 50
-lon = 30
-alt = 100
-hour = 12
-month = 1
 
-# Load the files
-sd_file = nc_utils.ncread_vars(in_fn_sd)
-d_file = nc_utils.ncread_vars(in_fn_d)
 
-# Get the dimensions  
-# (TODO add support for non-indexed values via interpolation)
-mi = d_file['month'] == month
-ai = d_file['lev'] == alt
-li = d_file['lat'] == lat
+def main(
+    # diurnal and semidiurnal tidal filenames
+    in_fn_sd = '~/Downloads/ctmt_semidiurnal_2002_2008.nc', 
+    in_fn_d = '~/Downloads/ctmt_diurnal_2002_2008.nc', 
 
-# Get the zonal component for DW1 (diurnal westward wavenumber 1, just as an example)
-# (TODO loop over all these for diurnal and semidiurnal)
-direction = 'w'     # east, west or stationary propagation (e, w, s)
-wavenumber = 1      # 0 - 4 (less in some cases)
-component = 'u'     # 'u' or 'v' (zonal or meridional aka eastward or northward)
-amp = d_file['amp_%s%i_%s' % (direction, wavenumber, component)][mi, ai, li]  #  amplitude (m/s) (east/west/north/up, depending on component)
-phase = d_file['phase_w1_u'][mi, ai, li]  # phase (UT of MAX at 0 deg lon)
+    # Location
+    lats = np.arange(-90, 95, 5),
+    lons = np.arange(0, 375, 15),
+    alt = 100, 
+    hour = 15, 
+    month = 9, 
+):
 
-phase_at_lon = phase - lon / 360 * 24   # Phase (UT of max at the specified lon). - sign used because westward. (eastward -> +, stationary means it doesn't move)
-wind = amp * np.cos((phase_at_lon - hour) / 24 * np.pi * 2)  #TODO Add support for wavenumber != 1 here
+    # Load the files
+    model = {'d':nc_utils.ncread_vars(in_fn_d), 's':nc_utils.ncread_vars(in_fn_sd)}
+    comps = table_of_components()
+
+    # generate the Oberheide figure
+    wind = []
+    lsts = [0, 6, 12, 18]
+    for lst in lsts:
+        wind.append(gen_oberheide_fig13(lats, lons, alt, month, model, comps, lst=lst, dirn='v'))
+
+    fig, ax = plt.subplots(1, 4, subplot_kw={'projection': ccrs.PlateCarree()})
+    fig.set_figheight(4)
+    fig.set_figwidth(16)
+    plt.set_cmap('jet')
+    for ind, lst in enumerate(lsts):
+        im = ax[ind].contourf(lons, lats, wind[ind], np.linspace(-57, 57, 11))
+        ax[ind].coastlines()
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.15, 0.2, 0.6, 0.05])
+    fig.colorbar(im, cax=cbar_ax, orientation='horizontal')
+    plt.show()
+
+
+    """
+    wind = winds_at_ut(lats, lons, alt, hour, month, model, comps)
+    fig, ax = plt.subplots(2, 1)
+    a1 = ax[0].contourf(lons, lats, wind['u'])
+    a2 = ax[1].contourf(lons, lats, wind['v']) 
+    fig.colorbar(a1, )
+    fig.colorbar(a2, )
+    plt.show()
+
+    """
+
+def gen_oberheide_fig13(lats, lons, alt, month, model, comps, lst=18, dirn='u'):
+    """
+    compare against https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2011JA016784
+    """
+    
+    # Calculate the wind
+    wind = np.zeros((len(lats), len(lons)))
+    hours = lst - lons / 360 * 24
+    for lonind, lon in enumerate(lons):
+        hour = hours[lonind]
+        for ds, comp_list in comps.items():
+            for comp in comp_list:
+                wind_comp, phase, amp = calc_wind(model[ds], lats, lon, alt, hour, month, comp, dirn, ds)
+                wind[:, lonind] = wind[:, lonind] + np.squeeze(wind_comp)
+
+    return wind
+
+
+def winds_at_ut(lats, lons, alt, hour, month, model, comps):
+    # Calculate the wind
+    zeros = np.zeros((len(lats), len(lons)))
+    wind = {'u': zeros.copy(), 'v': zeros.copy()}  
+    for ds, comp_list in comps.items():
+        for comp in comp_list:
+            for dirn in wind.keys():
+                wind_comp, phase, amp = calc_wind(model[ds], lats, lons, alt, hour, month, comp, dirn, ds)
+                wind[dirn] += wind_comp
+
+    return wind
+
+
+def calc_wind(model, lat, lon, alt, hour, month, component, direction, diurnal_semidiurnal='d'):
+    """ 
+    model: loaded diurnal or semidiurnal file
+    lat: scalar or vector latitude (must be subset of model['lat'])
+    lon: scalar or vector longitudes
+    component: 'e', 'w', 's' for east, west or stationary propagation + 0 - 4 for wavenumber
+    direction: 'u' or 'v' (zonal or meridional, aka east or north)
+
+    TODO: be careful about the meaning of the output (could be eastward, westward, northward or upward)
+    """
+
+    # Expand the query points into 2D arrays if necessary
+    lon, lat = np.meshgrid(lon, lat)
+    
+    # Get the dimensions  
+    mi = model['month'] == month
+    ai = model['lev'] == alt
+
+    # check all requested lat within model['lat']
+    assert np.all(np.in1d(lat, model['lat'])), "requested lat must be subset of model['lat']"
+
+    #  amplitude (m/s) (east/west/north/up, depending on component)
+    amp = np.interp(lat, model['lat'], model['amp_%s_%s' % (component, direction)][mi, ai, :].flatten())
+    #amp = CubicSpline(model['lat'], model['amp_%s_%s' % (component, direction)][mi, ai, :].flatten())(lat)
+
+    # phase (UT of MAX at 0 deg lon)
+    phase = np.interp(lat, model['lat'], model['phase_%s_%s' % (component, direction)][mi, ai, :].flatten())
+    #phase = CubicSpline(model['lat'], model['phase_%s_%s' % (component, direction)][mi, ai, :].flatten())(lat)
+   
+    # propagation direction multiplier used to determine phase at specified longitude
+    if component[0] == 'e': # eastward
+        dirn_multiplier = 1  
+    elif component[0] == 'w': # westward
+        dirn_multiplier = -1 
+    elif component[0] == 's': # stationary
+        dirn_multiplier = 0 
+    else:
+        raise Exception("Only 'e'/'w'/'s' components supported")
+
+    # diurnal/semidiurnal multiplier 
+    if diurnal_semidiurnal == 'd':
+        ds_multiplier = 1
+    elif diurnal_semidiurnal == 's':
+        ds_multiplier = 2
+    else:
+        raise Exception("Only 'd'/'s' diurnal/semidiurnal values supported")
+
+    # wavenumber multiplier
+    wn_multiplier = int(component[1])
+    assert np.abs(wn_multiplier) < 5, 'wavenumbers <= 4 supported'
+
+    # Phase (UT of max at the specified lon).
+    phase_at_lon = phase + dirn_multiplier * wn_multiplier * lon / 360 * 24    
+
+    # Wind values at the requested locations
+    wind = amp * np.cos((phase_at_lon - hour * ds_multiplier)  / 24 * np.pi * 2)  
+
+    return wind, phase, amp
+
+
+def table_of_components():
+    diurnal = ['w2', 'w1', 's0', 'e1', 'e2', 'e3',] 
+    semidiurnal = ['w4', 'w3',] + diurnal
+    
+    return {'d':diurnal, 's':semidiurnal}
+
+
+if __name__ == '__main__':
+    main()
 

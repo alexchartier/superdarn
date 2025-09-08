7fe489eb122f6843457d13d7f7ea8744c42ee89f
Alex Chartier <alex.chartier@outlook.com>
2025-09-08 10:50:02 -0400

    merge

--- winds/sd_height_resolved.m
+++ winds/sd_height_resolved.m
@@ -2,10 +2,13 @@
 % Calculate height-resolved meteor winds from fitACF data
 
 
-%% Inputs 
-in_fn = '/Users/chartat1/Downloads/20240510.cvw.despeck.nc';
+%% Inputs
+in_fn_fmt = '~/data/netcdf/{YYYYmmdd}.han.v3.0.nc';
+data_fn = '~/data/netcdf/sd.mat';
 
-times = datenum(2024, 1, 1):datenum(2024, 12, 31);
+preproc = false;
+
+times = datenum(2016, 1, 1):datenum(2016, 12, 31);
 hours = 0:23;
 heights = 70:10:110;
 
@@ -27,24 +30,47 @@ out.v_e = zarr;
 out.elv = zarr;
 
 
-%% Load 
-D = load_nc(in_fn);
-boresight = ncreadatt(in_fn, '/', 'boresight');
-beams = ncreadatt(in_fn, '/', 'beams');
+%% Load
+if preproc
+    loaded = 0;
+    for t = 1:length(times)
+        % Load daily file
+        in_fn = filename(in_fn_fmt, times(t));
+        if ~isfile(in_fn)
+            continue
+        end
 
+        boresight = ncreadatt(in_fn, '/', 'boresight');
+        beams = ncreadatt(in_fn, '/', 'beams');
+        Dt = load_nc(in_fn);
 
-%% Basic filtering
-center_beam = beams(floor(length(beams) / 2));
-% bi = beams == center_beam;
+        % center_beam = beams(floor(length(beams) / 2));
 
-D.time = D.mjd + datenum(1858, 11, 17);
-goodid = (D.v_e < MAX_V_ERR) & (abs(D.v) < MAX_VEL) & (D.w_l < MAX_W_L) ...
-    & (D.range < MAX_MET_RANGE) & ~D.gflg & (D.beam == center_beam);
+        % Index and downasmple
+        Dt.time = Dt.mjd + datenum(1858, 11, 17);
+        goodid = (Dt.v_e < MAX_V_ERR) & (abs(Dt.v) < MAX_VEL) & (Dt.w_l < MAX_W_L) ...
+            & (Dt.range < MAX_MET_RANGE) & ~Dt.gflg; % & (Dt.beam == center_beam);
 
-fields = fieldnames(D);
-for i = 1:length(fields)
-    D.(fields{i}) = D.(fields{i})(goodid);
+        fields = fieldnames(Dt);
+        for i = 1:length(fields)
+            Dt.(fields{i}) = Dt.(fields{i})(goodid);
+        end
+
+        % store in holder
+        if ~loaded
+            D = Dt;
+            loaded = 1;
+        else
+            for i = 1:length(fields)
+                D.(fields{i}) = [D.(fields{i}); Dt.(fields{i})];
+            end
+        end
+    end
+
+    savestruct(data_fn, D)
 end
+
+D = loadstruct(data_fn);
 
 
 %% Calculate heights (not perfect...)
@@ -53,35 +79,45 @@ fields = fieldnames(D);
 
 
 %% Loop over days
-    %% Loop over hours in the day
-    for hri = 1:length(hours)
-        ti = (D.time >= floor(min(D.time)) + (hours(hri) - 0.5) / 24 ) & ...
-            (D.time < floor(min(D.time)) + (hours(hri) + 0.5) / 24);
-
+% Note: Interferometer data is often complete nonsense
+times_hr = (times' + hours/24)';
+% times_hr = times_hr(:);
+zarr = zeros([size(times_hr)]) * NaN;
+out.v = zarr;
+out.counts = zarr;
+for di = 1:size(zarr, 1)
+    for hri = 1:size(zarr, 2)
+        ti = (D.time >= times_hr(di, hri) - (0.5 / 24)) & ...
+            (D.time < times_hr(di, hri) + (0.5 / 24));
+        clear Dt
         for i = 1:length(fields)
             Dt.(fields{i}) = D.(fields{i})(ti);
         end
+        % if sum(ti) > 0
+        %     disp(1)
+        % end
 
-        % Beam check
-        if length(unique(Dt.beam)) < MIN_BEAMS
-            continue
-        end
+        % % Beam check
+        % if length(unique(Dt.beam)) < MIN_BEAMS
+        %     continue
+        % end
 
-        % Store out
-        for htsi = 1:length(heights)
-            
-
-
-            out.v(di, hri, htsi) = median(valid_vels(ri))
-        end
+        out.counts(di, hri) = sum(ti);
+        out.v(di, hri) = median(Dt.v);
+        % for hti = 1:size(zarr, 3)
+        %     valid_vels = 
+        % 
+        % end
     end
+end
 
-%end
+
+
 
 %%
 % scatter(Dt.lon, Dt.lat, 100, Dt.v, 'filled')
-% 
-% %% 
+%
+% %%
 % plot(Dt.lon, Dt.lat, '.')
 
 
